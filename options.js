bb0c9c5b142e0eaddcf0ec289a30f4c7c6270f45
ProfessorHines <eric.hines@umontana.edu>
2016-02-24 20:32:35 -0700

    Update options.js

--- options.js
+++ options.js
@@ -1,10 +1,14 @@
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var ratio = document.getElementById('ratio').value;
+  var navScroll = document.getElementById('scroll').checked;
+  var navTouch = document.getElementById('touch').checked;
+  var navClick = document.getElementById('click').checked;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    presentationRatio: ratio,
+    navScroll: scroll
+    navTouch: touch
+    navClick: click
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -18,13 +22,17 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
+  // Use default values
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    presentationRatio: '4:3',
+    navScroll: true
+    navTouch: true
+    navClick: false
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('ratio').value = items.presentationRatio;
+    document.getElementById('scroll').checked = items.navScroll;
+    document.getElementById('touch').checked = items.navTouch;
+    document.getElementById('click').checked = items.navClick;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
